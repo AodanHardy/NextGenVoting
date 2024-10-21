518a2445ae5521edaa3f65f5d43284ff8e6e4cff
Aodan <hardy-a4@ulster.ac.uk>
2024-10-21 15:51:56 +0100

    added voted to voter model

--- voting/models.py
+++ voting/models.py
@@ -39,6 +39,7 @@ class Voter(models.Model):
     election = models.ForeignKey(Election, on_delete=models.CASCADE, related_name='voters')
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=False)
+    voted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
