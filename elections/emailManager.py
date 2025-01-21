e3c32b64638667516ab6116c77e6ca61f8a4c715
Aodan <hardy-a4@ulster.ac.uk>
2025-01-21 11:29:52 +0000

    added email stuff to env

--- elections/emailManager.py
+++ elections/emailManager.py
@@ -6,13 +6,10 @@ from django.template.loader import render_to_string
 from celery import shared_task
 
 from elections.models import Election
+from nextgenvoting.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST, EMAIL_PORT
 from voting.models import Voter
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nextgenvoting1@gmail.com'
-EMAIL_HOST_PASSWORD = 'ravw sviy arot ktlh'
+
 
 
 @shared_task
