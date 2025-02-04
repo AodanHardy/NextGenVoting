cb6a9ac6d65b23109d15123bc991710f46df3b75
Aodan <hardy-a4@ulster.ac.uk>
2025-02-04 13:58:40 +0000

    updated vote generater

--- voting/management/commands/generate_votes.py
+++ voting/management/commands/generate_votes.py
@@ -13,9 +13,16 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('num_votes', type=int, help="Number of votes to generate")
+        parser.add_argument('num_candidates', type=int, help="Number of Candidates max 15")
+        parser.add_argument('num_winners', type=int, help="Number of winners")
 
     def handle(self, *args, **options):
         num_votes = options['num_votes']
+        num_candidates = options['num_candidates']
+        if options['num_winners']:
+            num_winners = options['num_winners']
+        else:
+            num_winners = 1
 
         superuser = User.objects.get(id=1)
 
@@ -30,13 +37,19 @@ class Command(BaseCommand):
         ballot = Ballot.objects.create(election=election,
                                        title="test ballot",
                                        voting_type="RCV",
-                                       number_of_winners=3,
+                                       number_of_winners=num_winners,
                                        )
 
         # Create candidates
-        candiateNames = ["Michael","Jim","Dwight","Andy", "Oscar", "Creed", "Angela", "Pam"]
+        candidateNames = ["Michael", "Jim", "Dwight", "Andy", "Oscar", "Creed", "Angela", "Pam",
+                          "Toby", "Stanley", "Kevin", "Meredith", "Phyllis", "Ryan", "Jan"]
+
+        newNumber = num_candidates
+
+        userCandidates = random.sample(candidateNames, newNumber)
+
         candidates = [
-            Candidate.objects.create(ballot=ballot, title=candiateNames[i]) for i in range(len(candiateNames))
+            Candidate.objects.create(ballot=ballot, title=userCandidates[i]) for i in range(len(userCandidates))
         ]
 
         candidatesDict = {}
