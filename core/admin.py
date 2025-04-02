2cfc01ad63ab7e98772984e3a790e8a652db9bc4
Aodan <hardy-a4@ulster.ac.uk>
2025-04-02 07:50:17 +0100

    comments

--- core/admin.py
+++ core/admin.py
@@ -11,16 +11,19 @@ class ElectionAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'start_time', 'end_time')
     list_filter = ('status',)
     search_fields = ('title', 'description')
-    readonly_fields = ('start_time', 'end_time')  # Prevent editing after election has started/completed
+    readonly_fields = ('start_time', 'end_time')
+
 
 @admin.register(Ballot)
 class BallotAdmin(admin.ModelAdmin):
     list_display = ('title', 'voting_type', 'election')
     list_filter = ('voting_type', 'election')
 
+
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
     list_display = ('title', 'ballot')
+
 
 @admin.register(Voter)
 class VoterAdmin(admin.ModelAdmin):
